179d2274c95cf4faafcc4640104bd246e5f47443
aqwa <Aqwa1200@gmail.com>
2019-05-11 14:11:21 +0200

    Add files via upload

---
+++ modules.bat
@@ -0,0 +1,13 @@
+@echo off
+echo.Installation des dependances
+start "" /B npm install -- save discord.js
+start "" /B npm install -- save os
+start "" /B npm install -- save cpu-stat
+start "" /B npm install -- save systeminformation
+start "" /B npm install -- save speedtest-net
+start "" /B npm install -- save fs
+start "" /B npm install -- save chalk
+start "" /B npm install -- save physical-cpu-count
+start "" /B npm install -- save figlet
+start "" /B npm install -- save chalk-animation
+start "" /B npm install -- save enmap
