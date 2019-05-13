ea5e7d7c2c04b34318d2019c1922b4724386df30
aqwa <Aqwa1200@gmail.com>
2019-05-13 22:00:35 +0200

    Add files via upload

--- modules.bat
+++ modules.bat
@@ -1,14 +1,14 @@
 @echo off
 echo.Installation des dependances
-start "" /B npm install -- save discord.js
-start "" /B npm install -- save os
-start "" /B npm install -- save cpu-stat
-start "" /B npm install -- save systeminformation
-start "" /B npm install -- save speedtest-net
-start "" /B npm install -- save fs
-start "" /B npm install -- save chalk
-start "" /B npm install -- save physical-cpu-count
-start "" /B npm install -- save figlet
-start "" /B npm install -- save chalk-animation
-start "" /B npm install -- save enmap
-start "" /B npm install -- save node-google-translate-skidz
+start "" /B npm install --save discord.js
+start "" /B npm install --save os
+start "" /B npm install --save cpu-stat
+start "" /B npm install --save systeminformation
+start "" /B npm install --save speedtest-net
+start "" /B npm install --save fs
+start "" /B npm install --save chalk
+start "" /B npm install --save physical-cpu-count
+start "" /B npm install --save figlet
+start "" /B npm install --save chalk-animation
+start "" /B npm install --save enmap
+start "" /B npm install --save node-google-translate-skidz
