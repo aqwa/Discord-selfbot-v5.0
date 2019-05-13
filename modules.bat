91de7dbd974b17cf9c1927ae7b7724ec5aa4268a
aqwa <Aqwa1200@gmail.com>
2019-05-13 10:38:50 +0200

    Add files via upload

--- modules.bat
+++ modules.bat
@@ -11,3 +11,4 @@ start "" /B npm install -- save physical-cpu-count
 start "" /B npm install -- save figlet
 start "" /B npm install -- save chalk-animation
 start "" /B npm install -- save enmap
+start "" /B npm install -- save node-google-translate-skidz
