95a11a5895e4ceeb521ff7df68a8d7e380da3748
aqwa <Aqwa1200@gmail.com>
2019-05-28 17:42:05 +0200

    Add files via upload

--- commandes/exemple.js
+++ commandes/exemple.js
@@ -1,23 +1,23 @@
 // le nom de la commande sera prefix+nom du fichier ( exemple : !exemple ( vous pouvez changer ça depuis le fichier message.js))
 // si vous désirer une commande custom, créez un fichier dans ce même dossié et copiez le code si dessous.
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 exports.run = (self, message, args) => {
 
 	// Ne fait rien si vous n'êtes pas l'auteur du message
 	if(message.author.id !== self.user.id) {
 		return;
-	};
+	}
 	
 	// Suppression du message si vous en êtes l'auteur
 	if (message.author.id === self.user.id) { 			
 		message.delete();	
-	};
+	}
 	
 	// Exemple d'embed
 	var Exembed = new Discord.RichEmbed();
 	Exembed.setDescription("commsa");
 
-	message.channel.send(embed);
+	message.channel.send(Exembed);
 }
 
 var desc = "description commande exemple";
