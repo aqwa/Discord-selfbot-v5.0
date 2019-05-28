f77585b9b1e5b627c49c59a3fa3c7fe3d9e08c9e
aqwa <Aqwa1200@gmail.com>
2019-05-28 17:21:45 +0200

    Update exemple2.js

--- commandes/exemple2.js
+++ commandes/exemple2.js
@@ -3,13 +3,15 @@
 exports.run = (self, message, args) => {
 
     // Ne fait rien si vous n'êtes pas l'auteur du message
-    if(message.author.id !== self.user.id) return;
+    if(message.author.id !== self.user.id) {
+	    return;
+    }
 
     // Suppression du message si vous en êtes l'auteur
 	if (message.author.id === self.user.id) { 			
 		message.delete() 		
-    }
+    };
     
     // Réponse de la commande
     message.channel.send("Message");
-}
+};
