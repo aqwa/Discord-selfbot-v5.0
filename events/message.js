d0217f9f3267f3bda769cc2f1a2214eabe5d3c1c
aqwa <Aqwa1200@gmail.com>
2019-05-28 17:31:48 +0200

    Add files via upload

--- events/message.js
+++ events/message.js
@@ -1,7 +1,9 @@
 module.exports = (self, message) => {
   
     // Prefix
-    if (message.content.indexOf(self.conftoken.prefix) !== 0) return;
+    if (message.content.indexOf(self.conftoken.prefix) !== 0) {
+        return;
+    }
   
     // Définition de args et command
     const args = message.content.slice(self.conftoken.prefix.length).trim().split(/ +/g);
@@ -11,7 +13,9 @@ module.exports = (self, message) => {
     const cmd = self.commands.get(command);
   
     // Ne fait rien si la commande n'existe pas
-    if (!cmd) return;
+    if (!cmd) {
+        return;
+    }
   
     // Lance la commande si toute les conditions sont réunis
     cmd.run(self, message, args);
